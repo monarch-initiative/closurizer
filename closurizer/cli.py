33bb15a9369860190864576fcdb53878aec0a7fa
Kevin Schaper <kevinschaper@gmail.com>
2023-11-01 14:17:41 -0700

    Uses string_agg instead of array_agg to gather up closure ID and label lists to get pipe separated lists in the tsv. Also adds a dry_run flag to just print the query.

--- closurizer/cli.py
+++ closurizer/cli.py
@@ -9,12 +9,14 @@ from closurizer.closurizer import add_closure
 @click.option('--output', '-o', required=True, help='file write kgx file with closure fields added')
 @click.option('--fields', multiple=True, help='fields to closurize')
 @click.option('--grouping-fields', multiple=True, help='fields to populate a single value grouping_key field')
+@click.option('--dry-run', is_flag=True, help='A dry run will not write the output file, but will print the SQL query')
 def main(kg: str,
          closure: str,
          output: str,
+         dry_run: bool = False,
          fields: List[str] = None,
          grouping_fields: List[str] = None):
-    add_closure(kg_archive=kg, closure_file=closure, fields=fields, output_file=output, grouping_fields=grouping_fields)
+    add_closure(kg_archive=kg, closure_file=closure, fields=fields, output_file=output, dry_run=dry_run, grouping_fields=grouping_fields)
 
 if __name__ == "__main__":
     main()
