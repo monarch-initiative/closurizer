63af169c638c495b5881d2b5d97149a097f79d47
Kevin Schaper <kevinschaper@gmail.com>
2024-02-07 09:53:09 -0800

    tweak negation check again

--- closurizer/closurizer.py
+++ closurizer/closurizer.py
@@ -158,7 +158,7 @@ def add_closure(kg_archive: str,
         {node_columns('has_phenotype')}
     from nodes
         {node_joins('has_phenotype')}
-    where has_phenotype_edges.negated <> 'True'
+    where has_phenotype_edges.negated is null or has_phenotype_edges.negated = 'False'
     group by nodes.*
     """
     print(nodes_query)
